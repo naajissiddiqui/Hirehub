8c2c26ec08fbe802a90baee0fdc2b855c28c6980
naajissiddiqui <naajissiddiqui123@gmail.com>
2025-08-15 15:04:48 +0530

    updated onboarding page

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,30 +1,32 @@
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
 
+  const navigateUser = (currRole) => {
+    navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
+  };
+
   const handleRoleSelection = async (role) => {
-    if (!user) return;
-    try {
-      await user.update({
-        unsafeMetadata: { role },
+    await user
+      .update({ unsafeMetadata: { role } })
+      .then(() => {
+        console.log(`Role updated to: ${role}`);
+        navigateUser(role);
+      })
+      .catch((err) => {
+        console.error("Error updating role:", err);
       });
-      navigate(role === "recruiter" ? "/post-job" : "/jobs");
-    } catch (error) {
-      console.error("Failed to update user role:", error);
-    }
   };
 
   useEffect(() => {
-    if (isLoaded && user && user.unsafeMetadata?.role) {
-      navigate(
-        user.unsafeMetadata.role === "recruiter" ? "/post-job" : "/jobs"
-      );
+    if (user?.unsafeMetadata?.role) {
+      navigateUser(user.unsafeMetadata.role);
     }
   }, [user]);
 
@@ -33,12 +35,13 @@ const Onboarding = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mt-32">
+    <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
         I am a...
       </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
         <Button
+          variant="blue"
           className="h-36 text-2xl"
           onClick={() => handleRoleSelection("candidate")}
         >
