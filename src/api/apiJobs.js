5aca1af107beb34d141e718089cfcf7db8765ffb
naajissiddiqui <naajissiddiqui123@gmail.com>
2025-09-01 23:26:11 +0530

    minor changes

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -96,3 +96,20 @@ export async function updateHiringStatus(token, { job_id }, isOpen) {
 
   return data;
 }
+
+// - post job
+export async function addNewJob(token, _, jobData) {
+  const supabase = await supabaseClient(token);
+
+  const { data, error } = await supabase
+    .from("jobs")
+    .insert([jobData])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error Creating Job");
+  }
+
+  return data;
+}
--- src/pages/post-job.jsx
+++ src/pages/post-job.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import MDEditor from "@uiw/react-md-editor";
+import { State } from "country-state-city";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Navigate, useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  location: z.string().min(1, { message: "Select a location" }),
+  company_id: z.string().min(1, { message: "Select or Add a new Company" }),
+  requirements: z.string().min(1, { message: "Requirements are required" }),
+});
 const PostJob = () => {
-  return <div>PostJob</div>;
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { location: "", company_id: "", requirements: "" },
+    resolver: zodResolver(schema),
+  });
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
+        Post a Job
+      </h1>
+
+      <form>
+        <Input placeholder="Job Title" {...register(" title")} />
+        {errors.title && (
+          <p className="text-red-500">{errors.tittle.message}</p>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default PostJob;
